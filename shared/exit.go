edce12cbac1a3068459e77137868464aa4ade102
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-11-18 13:28:02 +0100

    feat: exit on bool

--- shared/exit.go
+++ shared/exit.go
@@ -10,6 +10,15 @@ func Exit(msg string, args ...any) {
 	os.Exit(0)
 }
 
+
+func ExitOnFalse(b bool, msg string, args ...any) {
+	logger := NewLogger("shared.ExitOnError")
+	if !b {
+		logger.Oops( fmt.Sprintf(msg, args...))
+		os.Exit(1)
+	}
+}
+
 func ExitOnError(err error, msg string, args ...any) {
 	logger := NewLogger("shared.ExitOnError")
 	if err != nil {
