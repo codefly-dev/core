30f6c67f3d299f8c53b44dcadcd2e09536e670d7
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-07-01 10:28:33 -0400

    fix: python files for lib

--- generated/python/codefly-cli/codefly_cli/tests/test_cli.py
+++ generated/python/codefly-cli/codefly_cli/tests/test_cli.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from codefly_cli.ci import Launcher
-from codefly_cli.codefly import with_dependencies, with_cli_logs, with_debug, with_code_path, with_silent
+from codefly_cli.ci import with_dependencies, with_cli_logs, with_debug, with_code_path, with_silent
 
 def test_nothing():
     assert True
