3e4efe44dc381a0c8dff3b5bbe9bea35ce6816b9
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-10-29 23:33:33 -0400

    update: companion version

--- scripts/build/companion.sh
+++ scripts/build/companion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for lang in go node proto python-poetry; do
-    # ./companions/$lang/scripts/tag.sh
+    ./companions/$lang/scripts/tag.sh
     ./companions/$lang/scripts/build_companion.sh
     ./companions/$lang/scripts/push_companion.sh
 done
